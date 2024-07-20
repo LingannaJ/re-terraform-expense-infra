a0602fe6f72c338cde9ffa8b88478c13bc3f1db5
LingannaJ <jlinganna104@gmail.com>
2024-07-20 23:32:47 +0530

    vp-jenkins

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,6 +1,6 @@
 module "aws_vpc" {
     # source = "../../re-terraform-aws-vpc"
-    source = "git::https://github.com/LingannaJ/re-terraform-expense-infra.git?ref=main"
+    source = "git::https://github.com/LingannaJ/re-terraform-aws-vpc.git?ref=main"
     common_tags = var.common_tags
     project_name = var.project_name
     environment = var.environment
