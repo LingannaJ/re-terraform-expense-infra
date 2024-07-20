db83811a64fc414ffdc971de647215636fb3e9e8
LingannaJ <jlinganna104@gmail.com>
2024-07-20 19:44:38 +0530

    jenkin-cicd

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,5 +1,6 @@
 module "aws_vpc" {
-    source = "../../re-terraform-aws-vpc"
+    # source = "../../re-terraform-aws-vpc"
+    source = "git::https://github.com/LingannaJ/re-terraform-expense-infra.git?ref=main"
     common_tags = var.common_tags
     project_name = var.project_name
     environment = var.environment
